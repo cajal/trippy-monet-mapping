526998ffbb3eb3fd2e287bea9b2fadf7db3980f7
Dimitri Yatsenko <dimitri@vathes.com>
2019-04-02 06:10:59 -0500

    optimization of datajoint data extraction

--- python/dj-scripts/extract_session.py
+++ python/dj-scripts/extract_session.py
@@ -12,51 +12,57 @@ import monet_trippy as mt
 # sessions that have both Monet and Trippy from a few recent experiments
 sessions = (fuse.Activity * stimulus.Sync & 'animal_id in (20505, 20322, 20457, 20210, 20892)'
             & (stimulus.Trial * stimulus.Monet2) & (stimulus.Trial * stimulus.Trippy)).fetch('KEY')
-key = sessions[0]   # pick one
+key = sessions[2]   # pick one
 
-print('load frame times.')
-pipe = (fuse.Activity() & key).module
-num_frames = (pipe.ScanInfo() & key).fetch1('nframes')
-num_depths = len(dj.U('z') & (pipe.ScanInfo.Field().proj('z', nomatch='field') & key))
-frame_times = (stimulus.Sync() & key).fetch1('frame_times', squeeze=True)  # one per depth
-assert num_frames <= frame_times.size / num_depths <= num_frames + 1
-frame_times = frame_times[:num_depths * num_frames:num_depths]  # one per volume
+for key in sessions:
+    folder = os.path.join(os.path.abspath('..'), 'data', 'sessions', dj.hash.key_hash(key)[:6])
+    if os.path.isdir(folder):
+        continue
 
-print('load and cache soma traces')
-trace_hash = dj.hash.key_hash({k: v for k, v in key.items() if k not in {'stimulus_type'}})
-archive = os.path.join('cache', trace_hash + '-traces.npz')
-if os.path.isfile(archive):
-    data = np.load(archive)
-    trace_keys = data['trace_keys']
-    traces = data['traces']
-    ms_delay = data['ms_delay']
-else:
-    units = pipe.ScanSet.Unit * pipe.MaskClassification.Type & {'type': 'soma'}
-    spikes = pipe.Activity.Trace * pipe.ScanSet.UnitInfo & units & key
-    trace_keys, traces, ms_delay = spikes.fetch('KEY', 'trace', 'ms_delay')
-    np.savez_compressed(archive, trace_keys=trace_keys, traces=traces, ms_delay=ms_delay)
-frame_times = np.add.outer(ms_delay / 1000, frame_times)  # num_traces x num_frames
+    print('load frame times.')
+    pipe = (fuse.Activity() & key).module
+    num_frames = (pipe.ScanInfo() & key).fetch1('nframes')
+    num_depths = len(dj.U('z') & (pipe.ScanInfo.Field().proj('z', nomatch='field') & key))
+    frame_times = (stimulus.Sync() & key).fetch1('frame_times', squeeze=True)  # one per depth
+    assert num_frames <= frame_times.size / num_depths <= num_frames + 1
+    frame_times = frame_times[:num_depths * num_frames:num_depths]  # one per volume
 
-print('create session and load trials')
-session = mt.VisualSession(np.stack(traces), frame_times)
+    print('load and cache soma traces')
+    trace_hash = dj.hash.key_hash({k: v for k, v in key.items() if k not in {'stimulus_type'}})
+    archive = os.path.join('cache', trace_hash + '-traces.npz')
+    if os.path.isfile(archive):
+        data = np.load(archive)
+        trace_keys = data['trace_keys']
+        traces = data['traces']
+        ms_delay = data['ms_delay']
+    else:
+        units = pipe.ScanSet.Unit * pipe.MaskClassification.Type & {'type': 'soma'}
+        spikes = pipe.Activity.Trace * pipe.ScanSet.UnitInfo & units & key
+        trace_keys, traces, ms_delay = spikes.fetch('KEY', 'trace', 'ms_delay')
+        np.savez_compressed(archive, trace_keys=trace_keys, traces=traces, ms_delay=ms_delay)
+    frame_times = np.add.outer(ms_delay / 1000, frame_times)  # num_traces x num_frames
 
-print('load trippy trials')
-for trial in tqdm((stimulus.Trial * stimulus.Condition * stimulus.Trippy & key).proj(..., '- movie')):
-    try:
-        session.add_trial(mt.Trippy.from_condition(trial), trial['flip_times'].flatten())
-    except IndexError:
-        warnings.warn('Invalid trial.')
+    print('create session and load trials')
+    session = mt.VisualSession(np.stack(traces), frame_times)
 
-print('load monet trials')
-for trial in tqdm((stimulus.Trial * stimulus.Condition * stimulus.Monet2 & key)):
-    try:
-        session.add_trial(mt.Monet2.from_condition(trial), trial['flip_times'].flatten())
-    except IndexError:
-        warnings.warn('Invalid trial.')
+    print('load trippy trials')
+    for trial in tqdm((stimulus.Trial * stimulus.Condition * stimulus.Trippy & key).proj(..., '- movie')):
+        try:
+            session.add_trial(mt.Trippy.from_condition(trial), trial['flip_times'].flatten())
+        except IndexError:
+            warnings.warn('Invalid trial.')
 
-folder = os.path.join(os.path.abspath('..'), 'data', 'sessions', dj.hash.key_hash(key)[:6])
-print('save session', folder)
+    print('load monet trials')
+    cache = {}
+    for trial in tqdm((stimulus.Trial * stimulus.Condition & stimulus.Monet2 & key)):
+        try:
+            if trial['condition_hash'] not in cache:
+                cache[trial['condition_hash']] = mt.Monet2.from_condition((stimulus.Condition * stimulus.Monet2 & trial).fetch1())
+            session.add_trial(cache[trial['condition_hash']], trial['flip_times'].flatten())
+        except IndexError:
+            warnings.warn('Invalid trial.')
 
-session.save(folder)
-with open(os.path.join(folder, 'cajal.json')) as f:
-    json.dump(key, f)
+    print('save session', folder)
+    session.save(folder)
+    with open(os.path.join(folder, 'cajal.json'), 'wt') as f:
+        json.dump(key, f)
